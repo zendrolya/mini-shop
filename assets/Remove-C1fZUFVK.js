5886de7eb80c4cb40eb6fe33e5ad1367d5fbc497
zendrolya <zendrolya@users.noreply.github.com>
2026-07-16 20:12:05 +0000

    deploy: 40b17cbf3d8fbc40d1246cb1759941ead8aa3361

---
+++ assets/Remove-C1fZUFVK.js
@@ -0,0 +1,1 @@
+import{g as e,ht as t}from"./index-D4AvFRe2.js";var n=t(),r=e((0,n.jsx)(`path`,{d:`M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z`}),`Delete`),i=e((0,n.jsx)(`path`,{d:`M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z`}),`Add`),a=e((0,n.jsx)(`path`,{d:`M19 13H5v-2h14z`}),`Remove`);export{i as n,r,a as t};
--- assets/useToast-aWefEe3Q.js
+++
@@ -1,1 +0,0 @@
-import{Ft as e,Pt as t,o as n}from"./index-B5W2fcBz.js";var r=e(t(),1);function i(){let e=(0,r.useContext)(n);if(!e)throw Error(`useToast должен использоваться в ToastProvider`);return e}export{i as t};
