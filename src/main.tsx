11601be12a2ce78955bb327723a7b483dd7a6467
Илья Дрозденко <drozdenkoid@gmail.com>
2026-07-17 00:03:43 +0400

    fix: hashRouter for gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ColorModeProvider } from './context/ColorModeContext';
 import ThemedApp from './components/ThemedApp';
@@ -11,11 +11,11 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <ColorModeProvider>
           <ThemedApp />
         </ColorModeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ export default defineConfig({
   server: {
     open: true,
   },
-  base: './',
+  base: '/mini-shop/',
 });
